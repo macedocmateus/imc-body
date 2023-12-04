c73a5aa8ff8a0d339ec4d3e2f0e3867c5fbca4d6
Mateus <mateusmacedoeng@gmail.com>
2023-12-04 16:41:43 -0300

    update ts

--- imc-body/script.js
+++ imc-body/script.js
@@ -1,3 +1,18 @@
 "use strict";
-let number = 5;
-console.log(number);
+// variáveis - variables
+// pegando informações para manipular depois
+const form = document.querySelector('form'); // o ponto de exclamação é para a constante pegar um tipo de dado diferente de null, no caso o const form pegou HTMLFormElement
+const inputWeight = document.querySelector('#weight'); // o 'as' funciona parecido com exclamação mas o tipo de dado é definido pelo programador
+const inputHeight = document.querySelector('#height');
+// 3 maneiras de criar e atribuir funções a um evento
+// 1
+form.onsubmit = function (event) {
+    event.preventDefault();
+    console.log(event);
+};
+// 2
+// form?.onsubmit = () => {}
+// 3
+// form?.onsubmit = handleSubmit
+// function handleSubmit() {
+// }
--- imc-body/script.ts
+++ imc-body/script.ts
@@ -1,3 +1,25 @@
-let number:number = 5;
+// variáveis - variables
 
-console.log(number);
+// pegando informações para manipular depois
+const form = document.querySelector('form') !; // o ponto de exclamação é para a constante pegar um tipo de dado diferente de null, no caso o const form pegou HTMLFormElement
+const inputWeight = document.querySelector('#weight') as HTMLInputElement; // o 'as' funciona parecido com exclamação mas o tipo de dado é definido pelo programador
+const inputHeight = document.querySelector('#height') as HTMLInputElement;
+
+// 3 maneiras de criar e atribuir funções a um evento
+
+// 1
+form.onsubmit  = function(event) { // padrão do submit em um formulário é enviar os dados e recarregar a página
+  
+  event.preventDefault();
+  
+  console.log(event);
+}
+
+// 2
+// form?.onsubmit = () => {}
+
+// 3
+// form?.onsubmit = handleSubmit
+// function handleSubmit() {
+
+// }
